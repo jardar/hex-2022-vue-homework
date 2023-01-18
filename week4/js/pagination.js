b8848dd4d440fa071b009ed97c159976160034e6
JarDar <jardar.tsai@gmail.com>
2023-01-18 11:23:25 +0800

    week 4 v1.3 add prevent modifier

--- week4/js/pagination.js
+++ week4/js/pagination.js
@@ -2,12 +2,12 @@ export default {
   template: `<nav aria-label="Page navigation">
   <ul class="pagination">
     <li :class="{'disabled': !page.has_pre}" class="page-item">
-      <a @click="handleChange(page.current_page-1)" class="page-link" href="#">上頁</a>
+      <a @click.prevent="handleChange(page.current_page-1)" class="page-link" href="#">上頁</a>
     </li>
-    <li v-for="p in page.total_pages"  :class="{'active':page.current_page===p}" class="page-item"><a @click="handleChange(p)" class="page-link" href="#">{{p}}</a></li>
+    <li v-for="p in page.total_pages"  :class="{'active':page.current_page===p}" class="page-item"><a @click.prevent="handleChange(p)" class="page-link" href="#">{{p}}</a></li>
     
     <li  :class="{'disabled': !page.has_next}" class="page-item">
-    <a @click="handleChange(page.current_page+1)" class="page-link" href="#">下頁</a></li>
+    <a @click.prevent="handleChange(page.current_page+1)" class="page-link" href="#">下頁</a></li>
   </ul>
 </nav>`,
   props: ["page"],
